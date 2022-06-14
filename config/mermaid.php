b1283d5fd5206ab58cc49f5ce8d71603675eb932
Luca Patera <lucapatera@outlook.it>
2022-06-14 22:32:10 +0200

    Add Laravel + Nutgram

---
+++ config/mermaid.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'baseurl' => env('MERMAID_BASEURL', 'http://localhost:8087/'),
+    'endpoint' => env('MERMAID_ENDPOINT', 'render.jpg'),
+    'tag' => env('MERMAID_TAG', '#mermaid'),
+];
---
+++ config/nutgram.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    // The Telegram BOT api token
+    'token' => env('TELEGRAM_TOKEN'),
+
+    // if the webhook mode must validate the incoming IP range is from a telegram server
+    'safe_mode' => env('APP_ENV', 'local') === 'production',
+
+    // Extra or specific configurations
+    'config' => [],
+
+    // Set if the service provider should automatically load
+    // handlers from /routes/telegram.php
+    'routes' => true,
+];
