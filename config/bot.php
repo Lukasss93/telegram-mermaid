5749ef1dc6174ef83769bac58044d0ac8492c465
Luca Patera <lucapatera@outlook.it>
2022-06-14 23:03:23 +0200

    Add configs: bot, owner

---
+++ config/bot.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'username' => env('BOT_USERNAME'),
+    'privacy' => env('BOT_PRIVACY'),
+    'channel' => env('BOT_CHANNEL'),
+    'source' => env('BOT_SOURCE'),
+    'changelog' => env('BOT_CHANGELOG'),
+];
---
+++ config/owner.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'id' => env('OWNER_ID'),
+    'channel' => env('OWNER_CHANNEL'),
+    'support' => env('OWNER_SUPPORT'),
+];
