f61032725add1f295ed9649a9285dab835949f1d
Luca Patera <lucapatera@outlook.it>
2022-11-07 19:29:30 +0100

    Add bot_name to nutgram config

--- config/nutgram.php
+++ config/nutgram.php
@@ -8,7 +8,9 @@ return [
     'safe_mode' => env('APP_ENV', 'local') === 'production',
 
     // Extra or specific configurations
-    'config' => [],
+    'config' => [
+        'bot_name' => env('BOT_USERNAME'),
+    ],
 
     // Set if the service provider should automatically load
     // handlers from /routes/telegram.php
