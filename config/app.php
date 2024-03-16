95093a410d3edf326e3f14ffd0d27a3a770417c7
Luca Patera <lucapatera@outlook.it>
2024-03-16 18:48:29 +0100

    Update app version

--- config/app.php
+++ config/app.php
@@ -18,13 +18,13 @@ return [
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
-     * --------------------------------------------------------------------------
-     * Application version
-     * --------------------------------------------------------------------------
-     * The version of your application. You may want to keep this updated in
-     * order to have correct version information in your application.
+     | --------------------------------------------------------------------------
+     | Application version
+     | --------------------------------------------------------------------------
+     | The version of your application. You may want to keep this updated in
+     | order to have correct version information in your application.
      */
-    'version' => '1.0.5',
+    'version' => '1.0.6',
 
     /*
     |--------------------------------------------------------------------------
